3c6b43669cd3179149a662983a94826d1172f552
Robert Taylor-Anderson <30276716+Robocop625@users.noreply.github.com>
2017-09-28 01:49:44 -0500

    Multiple views
    -Added stabilization to images
    -Twitter,IG & FaceBook icons added
    -Buttons change view

---
+++ Hooligan-App-Swift/FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Administrator on 9/26/17.
+//  Copyright © 2017 Tatlor'd Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
